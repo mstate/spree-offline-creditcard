1e0bf164b5c00079be1afd1cffbd3ff84cf928e6
Mike State <mstate@gmail.com>
2013-11-12 14:29:58 -0500

    added new store key fields for intializer file

--- lib/generators/spree_offline_credit_card/templates/initializer_for_offline_credit_card.rb
+++ lib/generators/spree_offline_credit_card/templates/initializer_for_offline_credit_card.rb
@@ -5,3 +5,9 @@ SpreeOfflineCreditCard::Config.public_key_email = 'my_public_key_email@example.c
 
 # path to your public key. e.g. 'config/my_public_key.asc'
 SpreeOfflineCreditCard::Config.public_key_path = 'config/my_public_key.asc'
+
+# email address associated with your store's key. e.g. 'my_store_key_email@example.com'
+SpreeOfflineCreditCard::Config.store_key_email = 'my_store_key_email@example.com'
+
+# path to your store's key file which contains the public and private keys. e.g. 'config/store_public_and_private_key.asc'
+SpreeOfflineCreditCard::Config.store_public_private_key_path = 'config/store_public_and_private_key.asc'
